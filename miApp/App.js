d2b7508201c360969ff2be24e5d16ce8e84cb4e6
elliderchoez <elliderchoez@gmail.com>
2026-01-07 23:30:48 -0500

    refactor: integrar AppProvider en App.js

--- miApp/App.js
+++ miApp/App.js
@@ -2,17 +2,20 @@ import { StatusBar } from 'expo-status-bar';
 import { AppNavigator } from './src/navigation/AppNavigator';
 import { ThemeProvider } from './src/context/ThemeContext';
 import { NotificationProvider } from './src/context/NotificationContext';
+import { AppProvider } from './src/context/AppContext';
 
 export default function App() {
   return (
-    <ThemeProvider>
-      <NotificationProvider>
-        <>
-          <AppNavigator />
-          <StatusBar style="auto" />
-        </>
-      </NotificationProvider>
-    </ThemeProvider>
+    <AppProvider>
+      <ThemeProvider>
+        <NotificationProvider>
+          <>
+            <AppNavigator />
+            <StatusBar style="auto" />
+          </>
+        </NotificationProvider>
+      </ThemeProvider>
+    </AppProvider>
   );
 }
 
